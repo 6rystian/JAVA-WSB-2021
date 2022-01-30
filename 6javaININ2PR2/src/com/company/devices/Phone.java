c043a4b634cade669cbbca4557fa10aaa02fc38d
6rystian <91750356+6rystian@users.noreply.github.com>
2022-01-30 19:17:44 +0100

    zadania 10.1 i 10.2
    zad10 do dokończenia

--- 6javaININ2PR2/src/com/company/devices/Phone.java
+++ 6javaININ2PR2/src/com/company/devices/Phone.java
@@ -4,7 +4,14 @@ import com.company.Device;
 import com.company.Human;
 import com.company.Saleable;
 
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.List;
+
 public class Phone extends Device implements Saleable {
+    private static final String DEFAULT_VERSION_NAME = "V1.0";
+    private static final String DEFAULT_SERVER_NAME = "wsb.gda.pl";
+    private static final String DEFAULT_PROTOCOL_NAME = "https";
     public Double screensize;
     public String os;
     public Integer year;
@@ -34,4 +41,35 @@ public class Phone extends Device implements Saleable {
         System.out.println("sus");
     }
 
+    public void installAnApp(String appName){
+        System.out.println("Instaluję aplikację "+appName);
+        this.installAnApp(appName, DEFAULT_VERSION_NAME);
+    }
+
+    public void installAnApp(String appName, String version){
+        System.out.println("Instaluję aplikację "+appName);
+        this.installAnApp(appName, version, DEFAULT_SERVER_NAME);
+    }
+
+    public void installAnApp(String appName, String version, String server){
+        URL url = null;
+        try {
+            url = new URL(DEFAULT_PROTOCOL_NAME, server, 443, appName + "-" + version);
+        } catch (MalformedURLException e) {
+            System.out.println("Przykro mi, podany adres nie istnieje.");
+            e.printStackTrace();
+        }
+        this.installAnApp(url);
+    }
+
+    public void installAnApp(List<String> appNames){
+        for (String appName : appNames) {
+            this.installAnApp(appName);
+        }
+    }
+
+    public void installAnApp(URL url){
+        //logika - sprawdzić miejsce na telefonie, czy apka jest płatna, obsługa płatności, ...
+        //...kontrola rodzicielska, obsługa błędów, pobranie, rozpakowanie, instalacja, dodanie ikony.
+    }
 }
